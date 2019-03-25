697e2e1284de721c0d924fb93fd06fcb0aaf81a3
SkObra <48929438+SkObra@users.noreply.github.com>
2019-03-25 21:16:38 +0000

    Initial commit

---
+++ APM/Form_Authentication.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace APM
+{
+    partial class Form_Authentication
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Authentication));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_UserName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_Password = new System.Windows.Forms.TextBox();
+            this.button_Login = new System.Windows.Forms.Button();
+            this.button_Exit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(147, 203);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User Name ";
+            // 
+            // textBox_UserName
+            // 
+            this.textBox_UserName.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_UserName.Location = new System.Drawing.Point(110, 233);
+            this.textBox_UserName.Name = "textBox_UserName";
+            this.textBox_UserName.Size = new System.Drawing.Size(151, 25);
+            this.textBox_UserName.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(307, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password ";
+            // 
+            // textBox_Password
+            // 
+            this.textBox_Password.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_Password.Location = new System.Drawing.Point(279, 233);
+            this.textBox_Password.Name = "textBox_Password";
+            this.textBox_Password.Size = new System.Drawing.Size(151, 25);
+            this.textBox_Password.TabIndex = 4;
+            // 
+            // button_Login
+            // 
+            this.button_Login.Font = new System.Drawing.Font("Cambria", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Login.Location = new System.Drawing.Point(207, 278);
+            this.button_Login.Name = "button_Login";
+            this.button_Login.Size = new System.Drawing.Size(54, 35);
+            this.button_Login.TabIndex = 5;
+            this.button_Login.Text = "Login";
+            this.button_Login.UseVisualStyleBackColor = true;
+            this.button_Login.Click += new System.EventHandler(this.button_Login_Click);
+            // 
+            // button_Exit
+            // 
+            this.button_Exit.Font = new System.Drawing.Font("Cambria", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Exit.Location = new System.Drawing.Point(279, 278);
+            this.button_Exit.Name = "button_Exit";
+            this.button_Exit.Size = new System.Drawing.Size(54, 35);
+            this.button_Exit.TabIndex = 6;
+            this.button_Exit.Text = "Exit";
+            this.button_Exit.UseVisualStyleBackColor = true;
+            this.button_Exit.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(173, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(180, 144);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form_Authentication
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(549, 360);
+            this.Controls.Add(this.button_Exit);
+            this.Controls.Add(this.button_Login);
+            this.Controls.Add(this.textBox_Password);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_UserName);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form_Authentication";
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_UserName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_Password;
+        private System.Windows.Forms.Button button_Login;
+        private System.Windows.Forms.Button button_Exit;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ APM/Form_Navigation.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace APM
+{
+    public partial class Form_Navigation : Form
+    {
+        /*string ConStr = Properties.Settings.Default.APM_DatabaseConnectionString;
+        SqlConnection APM_C = new SqlConnection(ConStr);*/
+     
+        SqlConnection APM_C = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\rpear\Documents\sK_oBrA\UOD\UnderGraduate\Year 2\Semester2\Team Project\Appointment Manager\Version0.3\APM\APM\APM_Database.mdf;Integrated Security=True");
+        
+        public Form_Navigation()
+        {
+            InitializeComponent();          
+        }
+        
+  
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        private void button_Save_Click(object sender, EventArgs e)
+        {         
+            try
+            {
+                if(APM_C.State == ConnectionState.Closed)
+                {
+                    APM_C.Open();
+                }
+               
+                SqlCommand Command1 = APM_C.CreateCommand();
+                Command1.CommandType = CommandType.Text;
+                Command1.CommandText = "insert into [CustomerP] (CustomerID, Name, Title, PostCode, MobileNumber, Email, ContactPreference) values ('" + textBox_CustID.Text + "','" + textBox_Name.Text + "','" + comboBox_Title.Text + "','" + textBox_PC.Text + "','" + textBox_MN.Text + "','" + textBox_Email.Text + "','" + comboBox_CP.Text + "')";
+                Command1.ExecuteNonQuery();
+                Command1.Parameters.Clear();
+
+                //textBox_CustID.Text = "";
+                textBox_Name.Text = "";
+                comboBox_Title.Text = "";
+                textBox_PC.Text = "";
+                textBox_MN.Text = "";
+                textBox_Email.Text = "";
+                comboBox_CP.Text = "";
+
+                SqlCommand Command2 = new SqlCommand();
+                Command2.Connection = APM_C;
+                Command2.CommandType = CommandType.Text;
+                Command2.CommandText = "insert into [CustomerV] (CarID, Manufacturer, Model, Transmission, LicensePlateID, Color, fkCustomerID) values ('" + textBox_CI.Text + "', '" + textBox_Man.Text + "', '" + textBox_Mod.Text + "', '" + comboBox_Tran.Text + "', '" + textBox_LP.Text + "', '" + textBox_VC.Text + "', '" + textBox_CustID.Text + "')";
+                Command2.ExecuteNonQuery();
+                Command2.Parameters.Clear();
+
+                // -- textBox_CI.Text = "";
+                textBox_Man.Text = "";
+                textBox_Mod.Text = "";
+                comboBox_Tran.Text = "";
+                textBox_LP.Text = "";
+                textBox_VC.Text = "";
+                textBox_CustID.Text = "";
+
+                SqlCommand Command3 = new SqlCommand();
+                Command3.Connection = APM_C;
+                Command3.CommandType = CommandType.Text;
+                Command3.CommandText = "insert into [Service] (ServiceID, DateS, Fault, Mechanic, CarID) values ('" + textBox_Sv.Text + "', '" + textBox_DP.Text + "','" + textBox_FL.Text + "', '" + textBox_MA.Text + "', '" + textBox_CI.Text + "')";
+                Command3.ExecuteNonQuery();
+                Command3.Parameters.Clear();
+                
+                // -- textBox_Sv.Text = "";
+                textBox_DP.Text = "";
+                textBox_FL.Text = "";
+                textBox_MA.Text = "";
+                textBox_CI.Text = "";
+
+                SqlCommand Command4 = new SqlCommand();
+                Command4.Connection = APM_C;
+                Command4.CommandType = CommandType.Text;
+                Command4.CommandText = "insert into [Invoice] (InvoiceID, ServiceID, TotalCost) values ('" + textBox_InD.Text + "','" + textBox_Sv.Text + "', '" + textBox_TC.Text + "')";
+                Command4.ExecuteNonQuery();
+                Command4.Parameters.Clear();
+                APM_C.Close();
+
+                textBox_InD.Text = "";
+                textBox_Sv.Text = "";
+                textBox_TC.Text = "";
+                MessageBox.Show("Data Saved Successfully");
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show(ex.Message,"Error! Please review the form");
+            }
+        }
+
+        public void dataLoader()
+        {
+            APM_C.Open();
+            SqlCommand commandLoad = APM_C.CreateCommand();
+            commandLoad.CommandType = CommandType.Text;
+            commandLoad.CommandText = "Select * from CustomerP, CustomerV, Service, Invoice";
+            commandLoad.ExecuteNonQuery();
+            DataTable tableInformation = new DataTable();
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(commandLoad);
+            dataAdapter.Fill(tableInformation);
+            dataGridView1.DataSource = tableInformation;
+            APM_C.Close();
+        }
+
+        private void button_Logout_Click(object sender, EventArgs e)
+        {
+            string message = "Are you sure?";
+            string title = "Caution!";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(message, title, buttons);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void button_StoredData_Click(object sender, EventArgs e)
+        {
+            dataLoader();
+        }
+    }
+}
